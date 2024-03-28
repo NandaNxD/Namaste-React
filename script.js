e394b4f1b461594ec8b9c2e11fbcf5b3d33619b0
NandanXDm <nanven9@gmail.com>
2024-03-28 12:38:08 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+const parent=React.createElement(
+    'div',{id:'parent'},
+    React.createElement(
+        'div',
+        {id:'child',className:'heading'},
+
+        React.createElement(
+            'h1',
+            {id:'child',className:'heading',},
+            'Im an h1'
+        ),
+        React.createElement(
+            'h1',
+            {id:'child',className:'heading',},
+            'Im an h1'
+        )       
+    )
+);
+
+
+
+const heading=React.createElement('h1',{id:'heading'},'hello world')
+console.log(heading)
+
+const root=ReactDOM.createRoot(document.getElementById('root'));
+console.log(root)
+
+root.render(parent)
